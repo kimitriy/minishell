2051d587e4791fe7342bb34adaf693933acd2633
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-04-14 00:40:51 +0300

    Add files via upload

--- split/split.c
+++ split/split.c
@@ -1,124 +1,78 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <ctype.h>
-#include <fcntl.h>
 
-size_t	ft_strlen(const char *s)
+int		**give_memory(int axlr[10000][2], int w)
 {
-	size_t	i;
+	int		**arr;
+	int		i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*substr;
-	size_t	i;
-	size_t	j;
-
-	if (start < 0 || len < 0 || s == NULL)
+	i = -1;
+	if (!(arr = (int**)malloc(w * sizeof(int*))))
 		return (NULL);
-	substr = (char*)malloc((len + 1) * sizeof(char));
-	if (NULL == substr)
-		return (NULL);
-	i = start;
-	j = 0;
-	while (i < (start + len) && start < ft_strlen(s))
+	i = -1;
+	while (++i < w)
 	{
-		substr[j] = s[i];
-		i++;
-		j++;
+		if (!(arr[i] = (int*)malloc(2 * sizeof(int))))
+			return (NULL);
+		arr[i][0] = axlr[i][0];
+		arr[i][1] = axlr[i][1];
 	}
-	substr[j] = '\0';
-	return (substr);
+	return (arr);
 }
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**arr;
-// 	size_t	wrdn;
-// 	size_t	i;
-// 	size_t	strt;
-
-// 	if (!s)
-// 		return (NULL);
-// 	i = -1;
-// 	wrdn = 0;
-// 	while (s[++i])
-// 		if ((s[i] == c && (s[i + 1] != c &&
-// 		s[i + 1] != 0)) || (s[i] != c && s[i + 1] == 0))
-// 			wrdn++;
-// 	if (!(arr = (char**)malloc((wrdn + 1) * sizeof(char*))))
-// 		return (NULL);
-// 	i = -1;
-// 	wrdn = 0;
-// 	strt = 0;
-// 	while (s[++i])
-// 		if (s[i] == c && s[i + 1] != c)
-// 			strt = i + 1;
-// 		else if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
-// 			arr[wrdn++] = ft_substr(s, strt, i - strt + 1);
-// 	arr[wrdn] = NULL;
-// 	return (arr);
-// }
-
-int		*wrd_cntr(char const *s, char c)
+int		**wrd_cntr(int *wn, char const *s, char c)
 {
-	int		i; //counter
+	int		i; //line symbol counter
+	int		w; //words(lines) counter
 	int		f; //flag
-	int		**axlr; //[0] - word number, [1] - start i, [2] - length
+	int		axlr[10000][2];
+	int		**arr;
 	
-	i = 0;
+	i = -1;
+	w = 0;
 	f = 0;
-	while (s[i])
+	while (s[++i])
 	{
-		if (s[i] != c && f == 0)
+		if (s[i] != c)
 		{
-			axlr[0]++;
-			axlr[1] = i;
-			f = 1;
+			if (f == 0 && (f = 1))
+				axlr[w][0] = i;
+			if (s[i + 1] == '\0' && (axlr[w][1] = i - axlr[w][0] + 1))
+				w++;
 		}
-		else if (s[i] == c)
-		{
-			axlr[2] = i - axlr[1];
+		else if (s[i] == c && f == 1 && (axlr[w++][1] = i - axlr[w][0]))
 			f = 0;
-		}
-		i++;
 	}
-	return (axlr);
+	arr = give_memory(axlr, w);
+	*wn = w;
+	return (arr);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	int		i;
+	int		k;
+	int		wn;
+	int		**axlr;
 	char	**arr;
-	int		**axlr; //[0] - word number, [1] - start i, [2] - end i
-	int		i; //line counter
-	int		j; //symb counter
 	
 	if (!s)
 		return (NULL);
-	axlr = wrd_cntr(s, c);
-	if (axlr[0] == 0)
+	axlr = wrd_cntr(&wn, s, c);
+	if (!(arr = (char**)malloc(wn * sizeof(char*))))
 		return (NULL);
-	if (!(arr = (char**)malloc(axlr[0] * sizeof(char*))))
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (i < axlr[0])
+	i = -1;
+	while (++i < wn)
 	{
-		while(s[l] != c)
-		{
-			arr[w][l] = c;
-			l++;
-		}
-		w++;
-		l = 0;
+		if (!(arr[i] = (char*)malloc(axlr[i][1] * sizeof(char))))
+			return (NULL);
+		k = -1;
+		while (++k < axlr[i][1])
+			arr[i][k] = s[k + axlr[i][0]];
 	}
+	arr[i] = NULL;
+	free(axlr);
 	return (arr);
 }
 
@@ -126,32 +80,32 @@ int		main(void)
 {
 	// while (1)
 	// {
-		// char	str[] = "   lorem   ipsum dolor  ";
-		char	str[] = "   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse 1";
+		char	str[] = "3   lorem   ipsum dolor     1";
+		// char	str[] = "   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse 1";
 		char		c;
 		char		**arrofp;
-		//size_t		i;	
+		int		i;	
 
 		c = ' ';
 		
 		arrofp = ft_split(str, c);
 		
-		/*
+		
 		i = 0;
-		while (arrofp[i])
+		while (arrofp[i] != NULL)
 		{
-			printf("arrofp[i]:%s\n", arrofp[i]);
+			printf("arrofp[%d]:%s\n", i, arrofp[i]);
 			i++;
 		}
-		*/
-		printf("arrofp[0]:%s\n", arrofp[0]);
-		printf("arrofp[1]:%s\n", arrofp[1]);
-		printf("arrofp[2]:%s\n", arrofp[2]);
-		printf("arrofp[3]:%s\n", arrofp[3]);
-		printf("arrofp[4]:%s\n", arrofp[4]);
-		printf("arrofp[5]:%s\n", arrofp[5]);
-		printf("arrofp[25]:%s\n", arrofp[25]);
-		printf("arrofp[26]:%s\n", arrofp[26]);
+		
+		// printf("arrofp[0]:%s\n", arrofp[0]);
+		// printf("arrofp[1]:%s\n", arrofp[1]);
+		// printf("arrofp[2]:%s\n", arrofp[2]);
+		// printf("arrofp[3]:%s\n", arrofp[3]);
+		// printf("arrofp[4]:%s\n", arrofp[4]);
+		// printf("arrofp[5]:%s\n", arrofp[5]);
+		// printf("arrofp[25]:%s\n", arrofp[25]);
+		// printf("arrofp[26]:%s\n", arrofp[26]);
 	// }
 	return (0);
 }
