ecb4f3e082bd25c0423bd5cfdbf7f9f53b48a786
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-05-14 09:04:27 +0400

    Add files via upload

--- misc/parse_arg.c
+++ misc/parse_arg.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*mem;
+
+	mem = s;
+	i = -1;
+	while (++i < n)
+		mem[i] = '\0';
+}
+
 int		ft_strlen(const char *s)
 {
 	int		i;
@@ -12,6 +23,28 @@ int		ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	if (NULL == s)
+		return (NULL);
+	i = 0;
+	if (c == '\0')
+	{
+		while (s[i])
+			i++;
+		return ((char*)&s[i]);
+	}
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char*)&s[i]);
+		i++;
+	}
+	return (NULL);
 }
 
 void	err_message(char *error)
@@ -28,18 +61,13 @@ void	print2darr(char **arr, int exprt_f)
 	int		i;
 
 	n = 0;
-	while (arr[n] != NULL)
-		n++;
 	i = -1;
-	while (++i < n)
+	while (arr[++i])
 	{
 		if (exprt_f != 0)
-			write(1, "declare -x ", 11);
-			// printf("declare -x ");
+			write(1, "declare -x ", 12);
 		write(1, arr[i], ft_strlen(arr[i]));
 		write(1, "\n", 1);
-		// printf("%s\n", arr[i]);
-		// printf("line:%d, %s\n", i, arr[i]);
 	}
 }
 
@@ -71,149 +99,208 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (sjn);
 }
 
-////ft_strtrim()
 
-size_t	lindx(char const *s1, char const *set)
+//////////ft_strtrim.c
+int	ft_check_set(char smb, char const *check)
 {
-	size_t	i;
-	size_t	j;
-	size_t	nomatch;
+	int	i;
 
-	i = -1;
-	nomatch = 0;
-	while (s1[++i])
+	i = 0;
+	while (check[i] != '\0')
 	{
-		j = -1;
-        while (set[++j])
-			if (s1[i] != set[j])
-				nomatch++;
-		if (nomatch < (size_t)ft_strlen(set))
-			nomatch = 0;
-		else
-			return (i);		
+		if (smb == check[i])
+			return (1);
+		i++;
 	}
 	return (0);
 }
 
-size_t	rindx(char const *s1, char const *set)
+char	*ft_strtrim(char const *str, char const *set)
 {
+	char	*trimstr;
 	size_t	i;
+	size_t	len;
 	size_t	j;
-	size_t	nomatch;
 
-	i = ft_strlen(s1);
-	nomatch = 0;
-	while (--i > 0)
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	len = ft_strlen(str);
+	while (str[i] != '\0' && ft_check_set(str[i], set))
+		i++;
+	if (i < len)
+		while (ft_check_set(str[len - 1], set))
+			len--;
+	trimstr = (char *)malloc(sizeof(*str) * (len - i + 1));
+	if (!trimstr)
+		return (NULL);
+	while (i + j < len)
 	{
-		j = -1;
-        while (set[++j])
-			if (s1[i] != set[j])
-				nomatch++;
-		if (nomatch < (size_t)ft_strlen(set))
-			nomatch = 0;
-		else
-			return (i);		
+		trimstr[j] = str[i + j];
+		j++;
 	}
-	return (0);
+	trimstr[j] = '\0';
+	return (trimstr);
 }
+// ////ft_strtrim()
 
-char	*ft_strtrim(char const *s1, char const *set)
+// int		lindx(char const *s1, char const *set)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		nomatch;
+
+// 	i = -1;
+// 	nomatch = 0;
+// 	while (s1[++i])
+// 	{
+// 		j = -1;
+//         while (set[++j])
+// 			if (s1[i] != set[j])
+// 				nomatch++;
+// 		if (nomatch < ft_strlen(set))
+// 			nomatch = 0;
+// 		else
+// 			return (i);		
+// 	}
+// 	return (0);
+// }
+
+// int		rindx(char const *s1, char const *set)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		nomatch;
+
+// 	i = ft_strlen(s1);
+// 	nomatch = 0;
+// 	while (--i > 0)
+// 	{
+// 		j = -1;
+//         while (set[++j])
+// 			if (s1[i] != set[j])
+// 				nomatch++;
+// 		if (nomatch < ft_strlen(set))
+// 			nomatch = 0;
+// 		else
+// 			return (i);		
+// 	}
+// 	return (0);
+// }
+
+// char	*ft_strtrim(char *s1, char const *set)
+// {
+// 	char	*strtrim;
+// 	int		i;
+// 	int		j;
+
+// 	if (s1 == NULL)
+// 		return (NULL);
+// 	i = rindx(s1, set);
+// 	j = lindx(s1, set);
+// 	strtrim = (char*)malloc((rindx(s1, set) - lindx(s1, set) + 2) * sizeof(char));
+// 	if (NULL == strtrim)
+// 		return (NULL);
+// 	i = -1;
+// 	j = lindx(s1, set) - 1;
+// 	while (++i < (rindx(s1, set) - lindx(s1, set) + 1))
+// 		strtrim[i] = s1[++j];
+// 	if (lindx(s1, set) == 0 && rindx(s1, set) == 0)
+// 		strtrim[0] = '\0';
+// 	strtrim[i] = '\0';
+// 	free(s1);
+// 	return (strtrim);
+// }
+
+// ////
+
+void	*ft_calloc(size_t count, size_t size)
 {
-	char	*strtrim;
-	size_t	i;
-	size_t	j;
+	void	*pntr;
 
-	if (s1 == NULL)
-		return (NULL);
-	strtrim = (char*)malloc((rindx(s1, set) - lindx(s1, set) + 2) * sizeof(char));
-	if (NULL == strtrim)
-		return (NULL);
-	i = -1;
-	j = lindx(s1, set) - 1;
-	while (++i < (rindx(s1, set) - lindx(s1, set) + 1))
-		strtrim[i] = s1[++j];
-	if (lindx(s1, set) == 0 && rindx(s1, set) == 0)
-		strtrim[0] = '\0';
-	strtrim[i] = '\0';
-	return (strtrim);
+	pntr = (void *)malloc(count * size);
+	if (NULL == pntr)
+		err_message("Memory allocation error! Exit!");
+	ft_bzero(pntr, count * size);
+	return (pntr);
 }
-
-////
 
 char	*fill_str(char *str, int len, int offset, int trm)
 {
 	char	*arr;
-	char	*tmp;
 	int		i;
 
-	arr = (char*)malloc((len + 1) * sizeof(char));
-	if (NULL == arr)
-		err_message("pzdts");
+	arr = ft_calloc(len + 1, sizeof(char));
+	// arr = (char*)malloc((len + 1) * sizeof(char));
+	// if (NULL == arr)
+	// 	err_message("pzdts");
 	i = -1;
 	while (++i < len)
 		arr[i] = str[offset + i];
 	arr[i] = '\0';
-	tmp = arr;
 	if (trm != 0)
 		arr = ft_strtrim(arr, "+");
-	free(tmp);
+	return (arr);
+}
+
+char	**parse_arg(char *str)
+{
+	char	**arr;
+	int		len;
+	int		offset;
+
+	arr = ft_calloc(3, sizeof(char*));
+	// arr = (char**)malloc(3 * sizeof(char*));
+	// if (NULL == arr)
+	// 	err_message("pzdts");
+	arr[2] = NULL;
+	len = 0;
+	while (str[len] && str[len] != 61)
+		len++;
+	arr[0] = fill_str(str, len, 0, 43);
+	if (NULL == ft_strchr(str, 61))
+		offset = len;
+	else
+		offset = len + 1;
+	len = 0;
+	while(str[offset + len])
+		len++;
+	arr[1] = fill_str(str, len, offset, 0);
 	return (arr);
 }
 
 // char	**parse_arg(char *str)
 // {
 // 	char	**arr;
+// 	char	*tmp;
 // 	int		len;
 // 	int		offset;
 
-// 	arr = (char**)malloc(3 * sizeof(char*));
+// 	arr = (char**)malloc(4 * sizeof(char*));
 // 	if (NULL == arr)
 // 		err_message("pzdts");
-// 	arr[2] = NULL;
+// 	arr[3] = NULL;
 // 	len = 0;
 // 	while (str[len] && str[len] != 61)
 // 		len++;
-// 	arr[0] = fill_str(str, len, 0);
+// 	arr[0] = fill_str(str, len, 0, 43);
+// 	arr[1] = fill_str(str, len, 0, 43);
+// 	tmp = arr[1];
+// 	arr[1] = ft_strjoin(arr[0], "=");
+// 	free(tmp);
 // 	offset = len;
 // 	offset++;
 // 	len = 0;
 // 	while(str[offset + len])
 // 		len++;
-// 	arr[1] = fill_str(str, len, offset);
+// 	arr[2] = fill_str(str, len, offset, 0);
 // 	return (arr);
 // }
 
-char	**parse_arg(char *str)
-{
-	char	**arr;
-	char	*tmp;
-	int		len;
-	int		offset;
-
-	arr = (char**)malloc(4 * sizeof(char*));
-	if (NULL == arr)
-		err_message("pzdts");
-	arr[3] = NULL;
-	len = 0;
-	while (str[len] && str[len] != 61)
-		len++;
-	arr[0] = fill_str(str, len, 0, 43);
-	arr[1] = fill_str(str, len, 0, 43);
-	tmp = arr[1];
-	arr[1] = ft_strjoin(arr[0], "=");
-	free(tmp);
-	offset = len;
-	offset++;
-	len = 0;
-	while(str[offset + len])
-		len++;
-	arr[2] = fill_str(str, len, offset, 0);
-	return (arr);
-}
-
 int     main()
 {
-    char    *str = "QW+=qwerty=123+";
+    char    *str = "_=/Applications/Visual Studio Code.app/Contents/MacOS/Electron";
     char    **arr;
 
     arr = parse_arg(str);
--- misc/str_in_arr.c
+++ misc/str_in_arr.c
@@ -276,126 +276,12 @@ char	*key_in_arr(char **arr, char *key)
 	return (NULL);
 }
 
-void	mark_str_to_del(char **arr, char *key)
+int     main()
 {
-	char	**prsd_arr;
-	int		i;
+    char    **arr;
+    char    *str;
 
-	i = 0;
-	while (arr[i])
-	{
-		prsd_arr = parse_arg(arr[i]);
-		if (0 == ft_strcmp(prsd_arr[0], key))
-		{
-			arr[i][0] = '\0';
-			ft_free(prsd_arr);
-			return ;
-		}
-		ft_free(prsd_arr);
-		i++;
-	}
-}
-
-char	**ft_rlcc_del(char **arr, int nsize)
-{
-	char	**narr;
-	int		i;
-	int		j;
-
-	narr = (char**)malloc((nsize + 1) * sizeof(char*));
-	if (NULL == narr)
-		err_message("realloc error");
-	i = -1;
-	j = 0;
-	while (arr[++i])
-	{
-		if (arr[i][0] != '\0')
-			narr[j] = ft_strdup(arr[i]);
-		else
-			continue;
-		j++;
-	}
-	narr[j] = NULL;
-	ft_free(arr);
-	return (narr);
-}
-
-char	**ft_rlcc_add(char **arr, int nsize, char *str)
-{
-	char	**narr;
-
-	narr = arr2d_copy(arr, nsize);
-	narr[nsize - 1] = ft_strdup(str);
-	return (narr);
-}
-
-char	**unst(char **arr, int osize, int nsize, char **prsd_str)
-{
-	char	**narr_1 = NULL;
-	char	**narr_2 = NULL;
-
-	narr_1 = arr2d_copy(arr, osize);
-	mark_str_to_del(narr_1, prsd_str[0]);
-	narr_2 = ft_rlcc_del(narr_1, nsize);
-	ft_free(prsd_str);
-	ft_free(arr);
-	return (narr_2);
-}
-
-void	str_ovrwrite(char *pstr, char *str)
-{
-	free(pstr);
-	pstr = NULL;
-	pstr = ft_strdup(str);
-	// return (pstr);
-}
-
-char	**exprt(char **arr, int nsize, char *str)
-{
-	char	**narr;
-
-	narr = ft_rlcc_add(arr, nsize, str);
-	ft_free(arr);
-	return(narr);
-}
-
-char**	ft_realloc(char **arr, int osize, int nsize, char *str)
-{
-	char	**narr;
-	char	**prsd_str;
-
-	prsd_str = parse_arg(str);
-	if (NULL != key_in_arr(arr, prsd_str[0])) //searches corresponding str upon the keys
-	{
-		if (nsize < osize) //соответствует unset
-		{
-			narr = unst(arr, osize, nsize, prsd_str);
-			return (narr);
-		}
-		else if (nsize == osize)
-			str_ovrwrite(key_in_arr(arr, prsd_str[0]), str);
-		ft_free(prsd_str);
-		return (arr);
-	}
-	else //there is no such key in the arr
-		if (osize < nsize) //соответствует export
-		{
-			narr = exprt(arr, nsize, str);
-			ft_free(prsd_str);
-			return(narr);
-		}
-	ft_free(prsd_str);
-	return (arr);
-}
-
-int		main()
-{
-	size_t	size = 4;
-	size_t	n_size = 3;
-	char	**arr;
-	// char	**tmp;
-
-	arr = (char**)malloc((size + 1) * sizeof(char*));
+    arr = (char**)malloc((5 + 1) * sizeof(char*));
 
 	arr[0] = ft_strdup("QW=111111");
 	arr[1] = ft_strdup("QWE=22222");
@@ -403,18 +289,7 @@ int		main()
 	arr[3] = ft_strdup("ER=444444");
 	arr[4] = NULL;
 
-	printf("before:\n");
-	print2darr(arr, 0);
-
-	arr = ft_realloc(arr, size, n_size, "WERT");
-	// arr = ft_realloc(arr, size, n_size, "TY=123456");
-	
-	printf("after:\n");
-	print2darr(arr, 0);
-	
-	while (1)
-	{
-
-	}
-	return (0);
+    print2darr(arr, 0);
+    str = key_in_arr(arr, "QWE");
+    printf("str: %s\n", str);
 }
