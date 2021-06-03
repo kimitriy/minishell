1ccdfdb9aa941e2f7d1ec7cb2f207e421ec1b7d2
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-06-03 14:04:00 +0400

    Add files via upload

--- mnshll/error.c
+++ mnshll/error.c
@@ -6,7 +6,7 @@
 /*   By: rburton <rburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 14:03:31 by rburton           #+#    #+#             */
-/*   Updated: 2021/05/21 10:14:01 by rburton          ###   ########.fr       */
+/*   Updated: 2021/06/03 13:02:53 by rburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	err_message(char *error)
     write(1, "Error!\n", 7);
 	write(1, error, ft_strlen(error));
     write(1, "\n", 1);
-    exit(0);
+    exit(1);
 }
 
 void	err_cmnd_not_fnd(t_set *s, int pi, int ci)
@@ -25,20 +25,20 @@ void	err_cmnd_not_fnd(t_set *s, int pi, int ci)
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
-	write(1, "command not found",  17);
-	write(1, "\n", 1);
+	write(1, "command not found\n",  18);
+	g_exit = 127;
 }
 
 void	err_not_a_valid_id(t_set *s, int pi, int ci)
 {
-	s->err = 1;
+	g_exit = 1;
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
 	write(1, s->st[pi].pln[ci].cmd[1], ft_strlen(s->st[pi].pln[ci].cmd[1]));
 	write(1, ": ", 2);
-	write(1, "not a valid identifier",  22);
-	write(1, "\n", 1);
+	write(1, "not a valid identifier\n",  23);
+	g_exit = 1;
 }
 
 void	err_home_not_set(t_set *s, int pi, int ci)
@@ -46,8 +46,8 @@ void	err_home_not_set(t_set *s, int pi, int ci)
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
-	write(1, "HOME not set",  12);
-	write(1, "\n", 1);
+	write(1, "HOME not set\n",  13);
+	g_exit = 1;
 }
 
 void	err_oldpwd_not_set(t_set *s, int pi, int ci)
@@ -55,33 +55,65 @@ void	err_oldpwd_not_set(t_set *s, int pi, int ci)
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
-	write(1, "OLDPWD not set",  12);
-	write(1, "\n", 1);
+	write(1, "OLDPWD not set\n",  15);
+	g_exit = 1;
 }
 
-void	err_no_such_file_or_directory(t_set *s, int pi, int ci)
+void	err_no_such_file_or_directory(t_set *s, int pi, int ci) //cd
 {
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
-	write(1, "No such file or directory",  25);
-	write(1, "\n", 1);
+	write(1, "No such file or directory\n",  26);
+	g_exit = 1;
 }
 
-void	err_numeric_arg_required(t_set *s, int pi, int ci)
+void	err_no_such_file_or_directory_env(t_set *s, int pi, int ci) //cd
 {
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
-	write(1, "numeric argument required",  25);
-	write(1, "\n", 1);
+	write(1, "No such file or directory\n",  26);
+	g_exit = 127;
 }
 
-void	err_too_many_arguments(t_set *s, int pi, int ci)
+void	err_numeric_arg_required(t_set *s, int pi, int ci) //exit
 {
 	write(1, "minishell: ",  11);
 	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
 	write(1, ": ", 2);
-	write(1, "too many argumnents",  19);
-	write(1, "\n", 1);
+	write(1, "numeric argument required\n",  26);
+	g_exit = 255;
+	exit(255);
+}
+
+void	err_too_many_arguments(t_set *s, int pi, int ci) //exit
+{
+	write(1, "minishell: ",  11);
+	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
+	write(1, ": ", 2);
+	write(1, "too many argumnents\n",  20);
+	g_exit = 1;
+}
+
+void	err_permission_denied(t_set *s, int pi, int ci) //exit
+{
+	// write(1, "minishell: ",  11);
+	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
+	write(1, ": ", 2);
+	write(1, s->st[pi].pln[ci].cmd[1], ft_strlen(s->st[pi].pln[ci].cmd[1]));
+	write(1, ": ", 2);
+	write(1, "Permission denied\n",  18);
+	g_exit = 1;
+}
+
+void	err_is_a_directory(t_set *s, int pi, int ci) //exit
+{
+	// write(1, "minishell: ",  11);
+	write(1, s->st[pi].pln[ci].cmd[0], ft_strlen(s->st[pi].pln[ci].cmd[0]));
+	write(1, ": ", 2);
+	write(1, s->st[pi].pln[ci].cmd[1], ft_strlen(s->st[pi].pln[ci].cmd[1]));
+	write(1, ": ", 2);
+	write(1, "Is a directory\n",  15);
+	g_exit = 1;
 }
--- mnshll/minishell.h
+++ mnshll/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rburton <rburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 12:15:35 by rburton           #+#    #+#             */
-/*   Updated: 2021/06/03 01:09:06 by rburton          ###   ########.fr       */
+/*   Updated: 2021/06/03 13:01:50 by rburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,68 @@ typedef struct	s_set
 	int			err;
 	int			shlvl;
 }				t_set;
+
+//global var
+int g_exit;
+
+// структура для листа ----------------------- ДОБАВИТЬ ДИМЕ
+
+typedef struct	s_list
+{
+	char			*data;
+	char			*dup;
+	struct s_list	*next;
+	struct s_list	*prev;
+}				t_list;
+
+//  структура под историю ----------------------- ДОБАВИТЬ ДИМЕ
+typedef struct	s_ter
+{
+	struct termios	term;
+	int				fd;
+	char 			*term_name;
+	int				symbols_count;
+	int				f;
+	t_list			*hist_list;
+	t_list			*current_hist_command;
+}				t_ter;
+
+// структура под парсер (доллар) ------------------- ДОБАВИТЬ ДИМЕ
+typedef struct s_dol
+{
+	short int		i;// index to walk through arg
+	char			*cont_symb;
+	short int		q;
+	short int		a;
+	short int		digit_aft_dol;
+	char			*path;
+	char			*env_var;
+	// char			*tmp;
+	char			*full_arg;
+	char			*in_env;
+	int				error;
+	int				redir;
+	int				red_out;// флаг на >
+	int				red_app;// флаг на >>
+	int				red_in; // флаг на <
+	int				fd_in;
+	int				fd_out;
+	char			*to_file; //куда записываем
+	char			*from_file; //откуда читаем
+}				t_dol;
+
+// структура для препарсера ----------------------Добавить ДИМЕ
+typedef struct	s_pars
+{
+	char			*changed;
+	char			*returned;
+	short int		q;
+	short int		a;
+	char			symb1;
+	char			symb2;
+	char			symb3;
+	char			symb4;
+}				t_pars;
 
 //main.c
 // int			main(int argc, char **argv, char **envp, char **apple);
