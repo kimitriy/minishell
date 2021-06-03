6f156b497944d8fa8f9206bf3eb1c88eb0a7c547
Smyriell <78075440+Smyriell@users.noreply.github.com>
2021-06-03 11:05:47 +0300

    KHV

--- preprsr.c
+++ preprsr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   preprsr.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smyriell <smyriell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rburton <rburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 19:42:37 by rburton           #+#    #+#             */
-/*   Updated: 2021/05/14 14:17:33 by smyriell         ###   ########.fr       */
+/*   Updated: 2021/04/22 21:27:38 by rburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 void ft_pre_splitting(char c, int q, int a) // подменяем для сплита
 {
 	if (c == ';' && (q == 1 || a == 1))
-		c = -1; // подмена точки с зап
+		c = 5; // подмена точки с зап
 	else if (c == ' ' && (q == 1 || a == 1))
-		c = -2; // подмена пробела
+		c = 8; // подмена пробела
 }
 
 void ft_aft_splitting(char c, int q, int a) // обратная подмена после сплита
 {
-	if (c == -1 && (q == 1 || a == 1))
+	if (c == 5 && (q == 1 || a == 1))
 		c = ';'; // подмена на точк с зап
-	else if (c == -2 && (q == 1 || a == 1))
+	else if (c == 8 && (q == 1 || a == 1))
 		c = ' '; // подмена на пробел
 }
 
@@ -57,39 +57,23 @@ void validation(char *str, int i)
 			write(2, "syntax error near unexpected token `>'", 39);
 		else if (str[i] == '<' && str[i + 1] == '<' && (q == 0 || a == 0)) // проверить поведение в кавычках
 			write(2, "bonus part", 39);
-			прямой и обратный редир рядом
-			рядом пайп и ;
-			после редиректа нельзя пайп ; и /0
-			>
-		bash: syntax error near unexpected token `newline' 
-			< ;
-		bash: syntax error near unexpected token `;'
-				>> >
-		bash: syntax error near unexpected token `>'
-			>< or <>
-	bash: syntax error near unexpected token `<'
-		
-		1>2  
-		
+		// else if (str[i] == '\\' && str[i + 1](q == 0 || a == 0)
+	
 		// condition for opened/closed quotes/apostr | ТАК ВРОДЕ ВСЕ РАБОТАЕТ НА ВАЛИДАЦИЮ КАВЫЧЕК ВО ВСЕЙ СТРОКЕ
 		
-		//--------------- ОБНОВИТЬ ДИМЕ ЭТОТ КУСОК-------------------//
 		if (str[i] == '\"' && str[i - 1] != '\\' && q == 0 && a == 0)
 			q = 1; // 1 - кавычка открылась	
 		else if (str[i] == '\"' && str[i - 1] != '\\' && q == 1)
 			q = 0;// 0 - кавычка закрылась
-		else if (str[i] == '\'' && str[i - 1] && a == 0 && q == 0)
+		else if (str[i] == '\'' && a == 0 && q == 0)
 			a = 1;
-		else if (str[i] == '\'' && str[i - 1] && a == 1) 
+		else if (str[i] == '\'' && a == 1) 
 			a = 0;
-		else if (i == len && (q != 0 || a != 0))  
+		if (i == len && (q != 0 || a != 0))
 			write(2, "//незакрытая кавычка/апостроф\n", 56);
-		else if (str[i] == '\\' && (str[i + 1] == '\0')) // || (str[i + 1] == '\"')))
-			write (2, "\\ at the end of argument not valid")
 		ft_pre_splitting(str[i], q, a);
 		i++;
 	}
-	return (0);
 }
 
 void lexer(char *str)
@@ -101,6 +85,6 @@ void lexer(char *str)
 		i++;
 	if (str[i] == ';' || str[i] == '|') // проверить поведение редиректов
 		write(2, ";// невалидный символ начала syntax error near unexpected token `;`\n", 91); //correct
-	if (validation(str, i))
-		write(2, "невалидный ввод", 30);
+	validation(str, i);
+	i++;
 }
